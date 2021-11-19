6e2b7c76d32d997b3c36d0f585c5ee61d35a49ff
Alejandro Reyes <arcaela.reyes@gmail.com>
2021-11-19 19:43:49 -0400

    PATCH

--- LocalBitcoin.php
+++ LocalBitcoin.php
@@ -46,11 +46,11 @@ class LocalBitcoin {
     }
 
     public function wallet($then=null, $catch=null){
-        return $this->_query("/api/wallet-balance/")->then(function(Object $response) use($then){
+        return $this->_query("/api/wallet-balance/")->then(function($data) use($then){
             $data = [
-                "address"=> $response->data? $response->data['receiving_address'] : 'UNKANOWM',
-                "balance"=>  $response->data? (float) $response->data['total']['balance'] : 0,
-                "available"=>  $response->data? (float) $response->data['total']['sendable'] : 0,
+                "address"=> $data['receiving_address'] ?? 'UNKANOWM',
+                "balance"=> (float) $data['total']['balance'] ?? 0,
+                "available"=> (float) $data['total']['sendable'] ?? 0,
             ];
             if( is_callable( $then )) return $then( $data );
             return $data;
