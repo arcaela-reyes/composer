b6b9e8ed555702e3472b57d61fafbea60967f6be
Alejandro Reyes <arcaela.reyes@gmail.com>
2021-11-22 00:17:56 -0400

    PATCH

--- Ajax.php
+++ Ajax.php
@@ -93,12 +93,10 @@ class Ajax {
             $this->curl_header['CURLOPT_POSTFIELDS'] = $this->input;
         } else {
             preg_match("/^([^?]+)(\?[^#]+)?/", $this->url, $match);
-            $url = $match[1];
-            $query = (is_array($this->input)?static::buildQuery($this->input):$this->input);
-            $query = count($match)>2 ? (
-                strlen($match[2])>1 ? $match[2] . "&${query}" : $query
-            ) : $query;
-            $this->url = $url . (strlen($query) ? "?${query}" : '');
+            if(isset($match[2])&&strlen($match[2])>1){
+                $query = $match[2] . '&' . (is_array($this->input)?static::buildQuery($this->input):$this->input);
+            } else $query = '?' . (is_array($this->input)?static::buildQuery($this->input):$this->input);
+            $this->url = $match[1] . $query;
             $this->curl_header['CURLOPT_HTTPGET'] = 1;
             $this->curl_header['CURLOPT_URL'] = $this->url;
         }
